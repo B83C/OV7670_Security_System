65534224199cef294cdf03e0619b65cc3f1c4ba1
B83C <b83c@outlook.com>
2024-12-21 17:29:38 +0800

    Final

---
+++ ips/managed_ip_project/managed_ip_project.cache/ip/2024.2/a/8/a889cefda94324d9/joystick_adc_stub.vhdl
@@ -0,0 +1,52 @@
+-- Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+-- Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2024.2 (lin64) Build 5239630 Fri Nov 08 22:34:34 MST 2024
+-- Date        : Sat Dec 21 14:15:15 2024
+-- Host        : t14s running 64-bit Arch Linux
+-- Command     : write_vhdl -force -mode synth_stub -rename_top decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix -prefix
+--               decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix_ joystick_adc_stub.vhdl
+-- Design      : joystick_adc
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a35tcpg236-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix is
+  Port ( 
+    daddr_in : in STD_LOGIC_VECTOR ( 6 downto 0 );
+    dclk_in : in STD_LOGIC;
+    den_in : in STD_LOGIC;
+    di_in : in STD_LOGIC_VECTOR ( 15 downto 0 );
+    dwe_in : in STD_LOGIC;
+    reset_in : in STD_LOGIC;
+    vauxp6 : in STD_LOGIC;
+    vauxn6 : in STD_LOGIC;
+    vauxp14 : in STD_LOGIC;
+    vauxn14 : in STD_LOGIC;
+    busy_out : out STD_LOGIC;
+    channel_out : out STD_LOGIC_VECTOR ( 4 downto 0 );
+    do_out : out STD_LOGIC_VECTOR ( 15 downto 0 );
+    drdy_out : out STD_LOGIC;
+    eoc_out : out STD_LOGIC;
+    eos_out : out STD_LOGIC;
+    vccaux_alarm_out : out STD_LOGIC;
+    vccint_alarm_out : out STD_LOGIC;
+    user_temp_alarm_out : out STD_LOGIC;
+    alarm_out : out STD_LOGIC;
+    vp_in : in STD_LOGIC;
+    vn_in : in STD_LOGIC
+  );
+
+  attribute CORE_GENERATION_INFO : string;
+  attribute CORE_GENERATION_INFO of decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix : entity is "joystick_adc,xadc_wiz_v3_3_11,{component_name=joystick_adc,enable_axi=false,enable_axi4stream=false,dclk_frequency=25,enable_busy=true,enable_convst=false,enable_convstclk=false,enable_dclk=true,enable_drp=true,enable_eoc=true,enable_eos=true,enable_vbram_alaram=false,enable_vccddro_alaram=false,enable_Vccint_Alaram=true,enable_Vccaux_alaram=true,enable_vccpaux_alaram=false,enable_vccpint_alaram=false,ot_alaram=false,user_temp_alaram=true,timing_mode=continuous,channel_averaging=None,sequencer_mode=on,startup_channel_selection=simultaneous_sampling}";
+end decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix;
+
+architecture stub of decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix is
+  attribute syn_black_box : boolean;
+  attribute black_box_pad_pin : string;
+  attribute syn_black_box of stub : architecture is true;
+  attribute black_box_pad_pin of stub : architecture is "daddr_in[6:0],dclk_in,den_in,di_in[15:0],dwe_in,reset_in,vauxp6,vauxn6,vauxp14,vauxn14,busy_out,channel_out[4:0],do_out[15:0],drdy_out,eoc_out,eos_out,vccaux_alarm_out,vccint_alarm_out,user_temp_alarm_out,alarm_out,vp_in,vn_in";
+begin
+end;
